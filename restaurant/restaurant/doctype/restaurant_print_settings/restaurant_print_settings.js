5e4b68baf8e0f0069008d3367a01b16cb9493a90
waddah82 <wmahnam@gmail.com>
2025-10-07 21:11:05 +0300

    Add files via upload

---
+++ restaurant/restaurant/doctype/restaurant_print_settings/restaurant_print_settings.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2022, Roque Vera and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Restaurant Print Settings', {
+	// refresh: function(frm) {
+
+	// }
+});
