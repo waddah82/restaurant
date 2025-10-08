f71ee401b273f31650875fca7f275b246961c17d
waddah82 <wmahnam@gmail.com>
2025-10-09 01:29:59 +0300

    Update gcs.bundle.js

--- restaurant/public/js/gcs.bundle.js
+++ restaurant/public/js/gcs.bundle.js
@@ -1,15 +1,33 @@
 window.onload = function() {
-
     setTimeout(applyCustomStyles, 500);
 };
 
 function applyCustomStyles() {
     const css = `
-             .widget.links-widget-box.link-item {
-        color: #180101 !important;
+             body, html {
+  background-color: #f9f9fb !important;
+  color: #222 !important;
+  font-family: 'Segoe UI', sans-serif !important;
+  font-size: 14px !important;
+}
+
+a {
+  color: #071879ff;
+}
+
+a:hover {
+  color: #020b38ff ;
+}
+
+::selection {
+  background-color: #d0d0ff ;
+}
+
+.widget.links-widget-box .link-item {
+    color: #180101 !important;
     font-weight: 500 !important;
     font-size: larger !important;
-    }
+}
     `;
     
     let style = document.createElement('style');
